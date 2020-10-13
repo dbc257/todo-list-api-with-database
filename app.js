0b40014a67bacaa9a864021c303ab563c2513c0c
David Carlisle <dave_0021@yahoo.com>
2020-10-13 13:26:09 -0500

    update

--- app.js
+++ app.js
@@ -1,23 +1,151 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./models');
+const es6Renderer = require('express-es6-template-engine');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const store = new SequelizeStore({ db: db.sequelize });
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret', // used to sign the cookie
+  resave: false, // update session even w/ no changes
+  saveUninitialized: true, // always create a session
+  store: store,
+}))
+
+store.sync();
+
+app.use((req, res, next) => {
+  console.log('===== USER =====')
+  console.log(req.session.user);
+  console.log('================')
+  next();
+})
 
 app.use(express.static('./public'));
 
-let todoList = [
-  {
-    id: 1,
-    todo: 'Implement a REST API',
-  },
-];
+app.engine('html', es6Renderer); // use es6renderer for html view templates
+app.set('views', 'templates'); // look in the 'templates' folder for view templates
+app.set('view engine', 'html'); // set the view engine to use the 'html' views
+
+function checkAuth(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+app.get('/', checkAuth, (req, res) => {
+  res.render('index', {
+    locals: {
+      user: req.session.user
+    }
+  });
+})
+
+app.get('/register', (req, res) => {
+  res.render('register', {
+    locals: {
+      error: null,
+    }
+  });
+})
+
+app.post('/register', (req, res) => {
+  // check if post was submitted with email and password
+  if (!req.body.email || !req.body.password) {
+    res.render('register', {
+      locals: {
+        error: 'Please submit all required fields'
+      }
+    })
+    return;
+  }
+
+  const { email, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    db.User.create({
+      email: email,
+      password: hash
+    })
+      .then((user) => {
+        res.redirect('/login');
+      })
+  })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', {
+    locals: {
+      error: null,
+    }
+  });
+})
+
+app.post('/login', (req, res) => {
+  // check if post was submitted with email and password
+  if (!req.body.email || !req.body.password) {
+    res.render('login', {
+      locals: {
+        error: 'Please submit all required fields'
+      }
+    })
+    return;
+  }
+
+  db.User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then((user) => {
+      if (!user) {
+        res.render('login', {
+          locals: {
+            error: 'No user with that email'
+          }
+        })
+        return;
+      }
+
+      bcrypt.compare(req.body.password, user.password, (err, matched) => {
+        if (matched) {
+          req.session.user = user;
+          res.redirect('/');
+        } else {
+          res.render('login', {
+            locals: {
+              error: 'Incorrect password. Please try again.'
+            }
+          })
+        }
+        return;
+      })
+    })
+})
+
+app.get('/logout', (req, res) => {
+  req.session.user = null;
+  res.redirect('/login');
+})
+
+app.use('/api*', checkAuth)
 
 // GET /api/todos
 app.get('/api/todos', (req, res) => {
-  db.Todo.findAll()
+  db.Todo.findAll({
+    where: {
+      UserId: req.session.user.id
+    }
+  })
     .then((todos) => {
       res.json(todos);
     })
@@ -54,7 +182,8 @@ app.post('/api/todos', (req, res) => {
   }
 
   db.Todo.create({
-    name: req.body.name
+    name: req.body.name,
+    UserId: req.session.user.id
   })
     .then((newTodo) => {
       res.json(newTodo);
